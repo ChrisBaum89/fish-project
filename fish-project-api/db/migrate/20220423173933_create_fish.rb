f88e41e5544dae99663a77829130ee569f49dbc5
Chris Baum <Christopher.Baum@valpo.edu>
2022-04-23 13:40:05 -0400

    created fish and category resources

---
+++ fish-project-api/db/migrate/20220423173933_create_fish.rb
@@ -0,0 +1,12 @@
+class CreateFish < ActiveRecord::Migration[6.1]
+  def change
+    create_table :fish do |t|
+      t.string :name
+      t.string :description
+      t.string :size
+      t.string :category_id
+
+      t.timestamps
+    end
+  end
+end
---
+++ fish-project-api/test/controllers/fish_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class FishControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
