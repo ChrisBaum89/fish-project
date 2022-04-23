ae660c204e609005013bbfe7372cbb24ab3b69f9
Chris Baum <Christopher.Baum@valpo.edu>
2022-04-23 15:43:06 -0400

    changed from Fishes to Fish

---
+++ fish-project-api/db/migrate/20220423173933_create_fish.rb
@@ -0,0 +1,13 @@
+class CreateFish < ActiveRecord::Migration[6.1]
+  def change
+    create_table :fish do |t|
+      t.string :name
+      t.string :description
+      t.string :size
+      t.string :img_url
+      t.integer :category_id
+
+      t.timestamps
+    end
+  end
+end
