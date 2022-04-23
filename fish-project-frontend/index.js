566143bed85a059ac4808b22e023393051f6265c
Chris Baum <Christopher.Baum@valpo.edu>
2022-04-23 15:32:05 -0400

    added fish tiles

--- fish-project-frontend/index.js
+++ fish-project-frontend/index.js
@@ -1,7 +1,19 @@
 document.addEventListener( "DOMContentLoaded", function () {
+  console.log('DOM loaded')
   addFishTiles()
+  addFishImages()
 });
 
 function addFishTiles(){
-  
+  for (let i = 1; i <= 6; i++){
+    const newSpan = document.createElement('div')
+    newSpan.setAttribute('id', 'rectangle')
+    //newSpan.setAttribute('id', `rectangle${i}`)
+    document.body.appendChild(newSpan)
+
+  }
+
+  function addFishImages(){
+
+  }
 }
