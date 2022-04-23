f8d15ee994bdd8338caae613fa571a6e3537d4d9
Chris Baum <Christopher.Baum@valpo.edu>
2022-04-23 13:54:00 -0400

    added index and show methods

--- fish-project-api/app/controllers/categories_controller.rb
+++ fish-project-api/app/controllers/categories_controller.rb
@@ -1,10 +1,13 @@
 class CategoriesController < ApplicationController
 
   def index
-    fish
+    categories = Category.all
+    render json: CategorySerializer.new(categories)
   end
 
   def show
-
+    category = Category.find(params[:id])
+    render json: CategorySerializer.new(category)
   end
+
 end
