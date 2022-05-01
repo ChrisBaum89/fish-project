99e543eb45cef5108fb2203c247860003ecdc997
Chris Baum <Christopher.Baum@valpo.edu>
2022-05-01 11:00:54 -0400

    added backgroundTiles and fishPictures

--- fish-project-frontend/index.js
+++ fish-project-frontend/index.js
@@ -1,29 +1,9 @@
 document.addEventListener( "DOMContentLoaded", function () {
   console.log('DOM loaded')
-  addFishTiles()
-  fetchCategories()
   fetchFish()
+  backgroundTiles()
+  fishPictures()
 });
-
-function addFishTiles(){
-  for (let i = 1; i <= 6; i++){
-    const newSpan = document.createElement('div')
-    newSpan.setAttribute('id', 'rectangle')
-    //newSpan.setAttribute('id', `rectangle${i}`)
-    document.body.appendChild(newSpan)
-    }
-  }
-
-function fetchCategories(){
-  fetch('http://localhost:3000/categories')
-  .then(function(response) {
-    console.log(response);
-    return response.json();
-  })
-  .then(function(json) {
-    console.log(json);
-  });
-}
 
 function fetchFish(){
   fetch('http://localhost:3000/fish')
@@ -32,6 +12,19 @@ function fetchFish(){
     return response.json();
   })
   .then(function(json) {
-    console.log(json);
+    console.log(json[0].name)
   });
 }
+
+function backgroundTiles(){
+  for (let i = 1; i <= 6; i++){
+    const newSpan = document.createElement('div')
+    newSpan.setAttribute('id', 'rectangle')
+    document.body.appendChild(newSpan)
+    }
+  }
+
+  function fishPictures(){
+    let fish = "fish"
+    console.log(fish)
+  }
