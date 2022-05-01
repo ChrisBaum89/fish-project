c76163d9eb15c081f5d18d8076126d4050779748
Chris Baum <Christopher.Baum@valpo.edu>
2022-05-01 11:59:00 -0400

    updated to get images to show up on tiles

--- fish-project-frontend/index.js
+++ fish-project-frontend/index.js
@@ -6,31 +6,31 @@ document.addEventListener( "DOMContentLoaded", function () {
 function fetchFish(){
   fetch('http://localhost:3000/fish')
   .then(function(response) {
-    console.log(response);
     return response.json();
   })
   .then(function(json) {
     backgroundTiles(json);
-    //fishPictures(json)
+    fishPictures(json)
   });
 }
 
 function backgroundTiles(json){
-  for (let i = 1; i <= json.length; i++){
-    const newSpan = document.createElement('div')
-    newSpan.setAttribute('id', 'rectangle' + i)
-    document.body.appendChild(newSpan)
+  for (let i = 0; i < json.length; i++){
+    const newDiv = document.createElement('div')
+    newDiv.setAttribute('class', 'rectangle')
+    newDiv.setAttribute('id', `rectangle${i}`)
+    console.log(newDiv)
+    document.body.appendChild(newDiv)
     }
   }
 
   function fishPictures(json){
-    for (let i = 1; i <= json.length; i++){
-      console.log(i)
-      var elementDiv = document.getElementById('rectangle'+i)
+    for (let i = 0; i < json.length; i++){
+      var elementDiv = document.getElementById(`rectangle${i}`)
       const newImg = document.createElement('img')
       newImg.src = json[i].img_url
       newImg.width = 100;
       newImg.height = 100;
-      document.getElementById('rectangle'+i).appendChild(newImg)
+      elementDiv.appendChild(newImg)
     }
   }
