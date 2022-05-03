a463255fdb5705b0b3d7ce58372233b1923573c2
Chris Baum <Christopher.Baum@valpo.edu>
2022-05-02 20:32:19 -0400

    added number_in_stock and price

--- fish-project-api/app/controllers/fish_controller.rb
+++ fish-project-api/app/controllers/fish_controller.rb
@@ -2,12 +2,12 @@ class FishController < ApplicationController
 
   def index
     fish = Fish.all
-    render json: fish.to_json(only: [:name, :description, :size, :img_url, :category_id])
+    render json: fish.to_json(only: [:name, :description, :size, :img_url, :category_id, :number_in_stock, :price])
   end
 
   def show
     fish = Fish.find_by_id(params[:id])
-    render json: fish.to_json(only: [:name, :description, :size, :img_url, :category_id])
+    render json: fish.to_json(only: [:name, :description, :size, :img_url, :category_id, :number_in_stock, :price])
   end
 
 end
