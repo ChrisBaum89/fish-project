557bdba440bc564ecd87053dcce072676886ac8d
Chris Baum <Christopher.Baum@valpo.edu>
2022-05-04 20:08:12 -0400

    adding id to show up on index page

--- fish-project-api/app/controllers/fish_controller.rb
+++ fish-project-api/app/controllers/fish_controller.rb
@@ -2,7 +2,7 @@ class FishController < ApplicationController
 
   def index
     fish = Fish.all
-    render json: fish.to_json(only: [:name, :description, :size, :img_url, :category_id, :number_in_stock, :price])
+    render json: fish.to_json(only: [:id, :name, :description, :size, :img_url, :category_id, :number_in_stock, :price])
   end
 
   def show
