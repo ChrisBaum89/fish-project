3a3e02a37e94c1336aa69b0f203c638a7ab4ccca
Chris Baum <Christopher.Baum@valpo.edu>
2022-05-10 20:08:09 -0400

    added index and show methods

--- fish-project-api/app/controllers/messages_controller.rb
+++ fish-project-api/app/controllers/messages_controller.rb
@@ -1,2 +1,18 @@
 class MessagesController < ApplicationController
+  def index
+    messages = Message.all
+    options = {
+      include: [:firstname, :lastname, :email, :phonenumber, :messagetext]
+    }
+    render json: MessageSerializer.new(messages)
+  end
+
+  def show
+    message = Message.find(params[:id])
+    options = {
+      include: [:firstname, :lastname, :email, :phonenumber, :messagetext]
+    }
+    render json: MessageSerializer.new(message)
+  end
+
 end
