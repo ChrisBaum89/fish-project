efb26d9ffdbc0afdd396b4143a58f183ac8edd69
Chris Baum <Christopher.Baum@valpo.edu>
2022-07-12 21:27:16 -0400

    added has_many relationship to fish

--- fish-project-api/app/serializers/fish_serializer.rb
+++ fish-project-api/app/serializers/fish_serializer.rb
@@ -2,4 +2,5 @@ class FishSerializer
   include FastJsonapi::ObjectSerializer
   attributes :id, :name, :description, :size, :vid_url, :category_id, :price, :number_in_stock
   belongs_to :category
+  has_many :fish
 end
