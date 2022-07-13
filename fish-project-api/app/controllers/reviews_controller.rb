5a839f80b6f8dc7c1a0f2333d7479fb5bde27de2
Chris Baum <Christopher.Baum@valpo.edu>
2022-07-12 20:18:34 -0400

    added index method

--- fish-project-api/app/controllers/reviews_controller.rb
+++ fish-project-api/app/controllers/reviews_controller.rb
@@ -1,2 +1,9 @@
 class ReviewsController < ApplicationController
+  def index
+    reviews = Review.all
+    options = {
+      include: [:name, :reviewtext, :stars]
+    }
+    render json: ReviewSerializer.new(messages)
+  end
 end
