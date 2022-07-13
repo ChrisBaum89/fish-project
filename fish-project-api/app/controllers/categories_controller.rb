9a08020c6809ca0efa74b15c05c59548fa05a7e7
Chris Baum <Christopher.Baum@valpo.edu>
2022-07-12 22:11:32 -0400

    change render structure to include reviews

--- fish-project-api/app/controllers/categories_controller.rb
+++ fish-project-api/app/controllers/categories_controller.rb
@@ -5,15 +5,17 @@ class CategoriesController < ApplicationController
     options = {
       include: [:fish]
     }
-    render json: CategorySerializer.new(categories, options)
+    render json: categories.to_json(only: [:name, :description], include: {fish:{include: {reviews: {}}}})
   end
 
   def show
     category = Category.find(params[:id])
+    #binding.pry
     options = {
       include: [:fish]
     }
-    render json: CategorySerializer.new(category, options)
+    #render json: CategorySerializer.new(category, options)
+    render json: category.to_json(only: [:name, :description], include: {fish:{include: {reviews: {}}}})
   end
 
 end
