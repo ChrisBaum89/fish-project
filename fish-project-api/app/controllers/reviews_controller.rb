cdf8b615ed29a26d59649217baf334514959db98
Chris Baum <Christopher.Baum@valpo.edu>
2022-07-12 20:31:20 -0400

    added to include more params and added Show and Create methods

--- fish-project-api/app/controllers/reviews_controller.rb
+++ fish-project-api/app/controllers/reviews_controller.rb
@@ -2,8 +2,27 @@ class ReviewsController < ApplicationController
   def index
     reviews = Review.all
     options = {
-      include: [:name, :reviewtext, :stars]
+      include: [:name, :reviewtext, :stars, :fish_id, :timestamps, :created_at, :updated_at]
     }
     render json: ReviewSerializer.new(reviews)
+  end
+
+  def show
+    review = Review.find(params[:id])
+    options = {
+      include: [:name, :reviewtext, :stars, :fish_id, :timestamps, :created_at, :updated_at]
+    }
+    render json: ReviewSerializer.new(review)
+  end
+
+  def create
+    @review = Review.new(
+      name: params[:name],
+      reviewtext: params[:reviewtext],
+      stars: params[:stars],
+      fish_id: params[:fish_id]
+    )
+    @review.save
+    render json: ReviewSerializer.new(@review)
   end
 end
