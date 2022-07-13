29825f135a68ccf9306f56f27f38e821fc5ba073
Chris Baum <Christopher.Baum@valpo.edu>
2022-07-12 21:27:05 -0400

    added reviews to json

--- fish-project-api/app/controllers/fish_controller.rb
+++ fish-project-api/app/controllers/fish_controller.rb
@@ -2,12 +2,18 @@ class FishController < ApplicationController
 
   def index
     fish = Fish.all
-    render json: fish.to_json(only: [:id, :name, :description, :size, :img_url, :category_id, :number_in_stock, :price])
+    options = {
+      include: [:reviews]
+    }
+    render json: fish.to_json(only: [:id, :name, :description, :size, :img_url, :category_id, :number_in_stock, :price], include: {reviews: []})
   end
 
   def show
     fish = Fish.find_by_id(params[:id])
-    render json: fish.to_json(only: [:name, :description, :size, :img_url, :category_id, :number_in_stock, :price])
+    options = {
+      include: [:reviews]
+    }
+    render json: fish.to_json(only: [:name, :description, :size, :img_url, :category_id, :number_in_stock, :price, :reviews], include: {reviews: []})
   end
 
 end
