59a4d4cb4bc1a329dd8d387a26d356b6da328a80
Chris Baum <Christopher.Baum@valpo.edu>
2022-07-12 20:17:10 -0400

    added name,reviewtext, and stars categories

--- fish-project-api/db/migrate/20220713001108_create_reviews.rb
+++ fish-project-api/db/migrate/20220713001108_create_reviews.rb
@@ -1,6 +1,9 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
   def change
     create_table :reviews do |t|
+      t.string :name
+      t.string :reviewtext
+      t.integer :stars
 
       t.timestamps
     end
